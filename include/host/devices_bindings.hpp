d3a650b7c7129e40f6465cccb7a8f971c9e7e813
vergelli <federico_joelvergelli@hotmail.com>
2025-05-15 17:39:43 -0300

    Add bindings for Devices class using pybind11

---
+++ include/host/devices_bindings.hpp
@@ -0,0 +1,9 @@
+#ifndef DEVICES_BINDINGS_CUH
+#define DEVICES_BINDINGS_CUH
+
+#pragma once
+#include <pybind11/pybind11.h>
+
+void bind_devices(pybind11::module_&);
+
+#endif
---
+++ src/host/bindings/devices_bindings.cpp
@@ -0,0 +1,14 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>  // permite usar std::vector y std::unordered_map
+#include "devices.cuh"
+#include "devices_bindings.hpp"
+
+
+namespace py = pybind11;
+
+void bind_devices(py::module_& m) {
+    py::class_<Devices>(m, "Devices")
+        .def(py::init<>())
+        .def("get_devices", &Devices::get_devices)
+        .def("print_devices", &Devices::print_devices);
+}
