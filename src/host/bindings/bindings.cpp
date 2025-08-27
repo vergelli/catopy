f39c42b2ae673007ed32a7abebbbb3c39bb9c598
vergelli <federico_joelvergelli@hotmail.com>
2025-08-27 12:17:52 -0300

    feat(feat/vecInit-1): add data structures bindings call

--- src/host/bindings/bindings.cpp
+++ src/host/bindings/bindings.cpp
@@ -1,7 +1,13 @@
 #include <pybind11/pybind11.h>
 #include "devices_bindings.hpp"
+#include "data_structures_bindings.hpp"
 
 PYBIND11_MODULE(cato, m) {
     m.doc() = "Cato CUDA module";
+
+    // Bind device detection functionality
     bind_devices(m);
+
+    // Bind data structures (vectors, matrices, tensors)
+    bind_data_structures(m);
 }
